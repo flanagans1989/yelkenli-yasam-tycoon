312ab01e007578cf98c7400abc9aacbbb7aa8954
flanagans1989 <deraksizolasyon@gmail.com>
2026-05-07 07:25:06 +0300

    Add Capacitor base configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+﻿import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.deraks.yelkenliyasamtycoon',
+  appName: 'Yelkenli Yasam Tycoon',
+  webDir: 'dist'
+};
+
+export default config;
